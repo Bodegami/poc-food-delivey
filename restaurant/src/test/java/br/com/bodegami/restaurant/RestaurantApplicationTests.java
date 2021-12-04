f8e4037160d21d54acdb667c0f36e50b14090693
Renato Ferreira Oliveira <65985509+Bodegami@users.noreply.github.com>
2021-12-04 11:48:43 -0300

    setup inicial

---
+++ restaurant/src/test/java/br/com/bodegami/restaurant/RestaurantApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bodegami.restaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
